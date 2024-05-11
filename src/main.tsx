6a9cbb0ed754fd89294a46f68d3788379506b848
Mariia Ionova <ionova_1999@list.ru>
2024-05-11 17:29:48 +0300

    feat: add theme, fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.module.scss';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@emotion/react';
+import theme from './vendor/theme.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <CssBaseline />
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
