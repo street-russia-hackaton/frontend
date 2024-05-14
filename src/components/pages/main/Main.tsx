d78e55661be8811895befbbbb796a1d948120c1e
Mariia Ionova <ionova_1999@list.ru>
2024-05-14 20:57:05 +0300

    feat: add intro, running line

--- src/components/pages/main/Main.tsx
+++ src/components/pages/main/Main.tsx
@@ -1,12 +1,22 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { Container, Typography } from '@mui/material';
+import React from 'react';
 import Header from '../../common/header/Header';
+import Intro from './intro/Intro';
+import Marquee from '../../common/marquee/Marquee';
+
+const contentArr = [
+    `• доступность • общность • здоровье • открытость • новаторство • доступность • общность • здоровье • открытость • новаторство `,
+    '• RAP • BREAKING • SKATEBOARDING • BMX • SCOOT • STREET ART • PARKOUR • WORKOUT • RAP • BREAKING • SKATEBOARDING • BMX • SCOOT • STREET ART • PARKOUR • WORKOUT '
+]
 
 export default function Main() {
+
     return (
         <>
-        <Header />
-            <Typography>Hello</Typography>
+            <Header backgroundColor='rgba(255,255,255, 0.27)' backdropFilter='blur(10px)' />
+            <Intro />
+            <Marquee color='#fff' background='#AA433A' content={contentArr[0]} rotate='rotate(8.1deg)' marginTop='-116px'/>
+                <Marquee color='#222' background='#D2D1D0' content={contentArr[1]} rotate='rotate(-4.9deg)' marginTop='-10px'/>
         </>
     );
 }
---
+++ src/components/pages/main/intro/Intro.tsx
@@ -0,0 +1,25 @@
+import { Box, Container, Typography } from '@mui/material';
+import React from 'react';
+import Background from '../../../../assets/images/main-page-background.png';
+import Logo from '../../../../assets/images/logo-main.png';
+import SubmitButton from '../../../btns/SubmitBtn';
+
+export default function Intro() {
+    const handleClick = () => {
+        // TODO: click
+    };
+
+    return (
+        <>
+            <Container sx={{position: 'relative', background: `no-repeat center url(${Background})`, backgroundSize: 'cover',  height: '852px', objectFit: 'cover', maxWidth: { lg: '100%' }, p: { sm: 0 } }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: '1350px', gap: '183px', alignItems: 'end', margin: '0 auto', p: '173px 112px 0 0', boxSizing: 'border-box' }}>
+                    <Box sx={{alignSelf: 'start'}}>
+                        <img src={Logo} alt="Логотип." />
+                        <Typography sx={{ fontFamily: 'Benzin', fontSize: '20px', fontWeight: 400, lineHeight: '1.5', letterSpacing: '0.02em', color: '#fff', pt: '10px' }}>Общероссийская общественная организация</Typography>
+                    </Box>
+                    <SubmitButton width="203px" title="Вступить" height="54px" fontSize="20px" fontWeigth="400" backgroundColor="#2E5B97" disabled={false} color="#fff" border="none" hoverBackgroundColor="#F2F3F5" focusBackgroundColor="#F2F3F5" textTransform="uppercase" lineHeight="1.5" onClick={handleClick} />
+                </Box>
+            </Container>
+        </>
+    );
+}
