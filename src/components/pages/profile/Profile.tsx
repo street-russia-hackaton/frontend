593edefa3b7483f1a90e9a1dd024a0079586e823
Maria <ionova_1999@list.ru>
2024-05-27 22:45:55 +0300

    fix: add objects to profile

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -1,13 +1,28 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, List, ListItem } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import Header from '../../common/header/Header';
 import Footer from '../../common/footer/Footer';
 import Title from '../../common/titleMainPage/titleMainPage';
-import { userData } from '../../../utils/constants';
+import { userData, objectCardData } from '../../../utils/constants';
 import ProfileCard from '../../common/profile/ProfileCard';
 import ProfileEvents from '../../common/profile/ProfileEvents';
 import SubmitBtnWithIcon from '../../common/btns/SubmitBtnWithIcon';
 import ProfileNews from '../../common/profile/ProfileNews';
+import ObjectCard from '../../common/objects/ObjectCard';
+
+const styles = {
+    cardContainer: {
+        p: '40px 0 40px 0',
+        width: '100%',
+        display: 'grid',
+        gap: '32px',
+    },
+    listItem: {
+        p: 0,
+        width: 'auto',
+        display: 'block',
+    },
+};
 
 export default function Profile() {
     const { state } = useLocation();
@@ -37,7 +52,13 @@ export default function Profile() {
                         <Title title={titleObject} background="none" margin="0 0 64px 0" width="100%" height="100%" padding="0" color="#fff" />
                     <SubmitBtnWithIcon title="создать ОбЪект" width="260px"/>
                     </Box>
-                    {/* TODO добавить обьекты */}
+                    <List sx={{ ...styles.cardContainer }}>
+                    {objectCardData.slice(1, 4).map((data) => (
+                        <ListItem key={data.id ?? 0} sx={{ ...styles.listItem }}>
+                            <ObjectCard data={data} />
+                        </ListItem>
+                    ))}
+                </List>
                 </Container>}
                 {!isUser && <Container component="section" sx={{ display: 'flex', flexDirection: 'column', p: { sm: '0' }, m: '0 auto 64px', width: '100%', maxWidth: { lg: '1320px' } }}>
                     <Box sx={{ display: 'flex', justifyContent: 'space-between', p: { sm: '0' }, m: '00' }}>
